1f37700abd58739dd58b2a936d595ad7b98bce29
Jon Uhlmann <account@uhlmann.pro>
2022-03-29 15:09:54 +0200

    Breaking: Next version - Change license to GPL-3.0 - Replace Sass with PostCSS - Adjust styling with custom properties - Update A11yDialog - Remove rendered inline assets - Replace build stack - Add code of conduct

--- Resources/Private/Assets/Dialog.js
+++ Resources/Private/Assets/Dialog.js
@@ -1,4 +1,4 @@
-import A11yDialog from './_A11yDialog';
+import A11yDialog from "a11y-dialog";
 
 let dialogues = [];
 
@@ -7,26 +7,24 @@ function domReady(callback) {
 
     function runCallback(event) {
         if (firstRun) {
-            if (typeof callback == 'function') {
+            if (typeof callback == "function") {
                 callback(event);
             }
             firstRun = false;
         }
     }
 
-    if (document.readyState == 'loading') {
-        document.addEventListener('readystatechange', runCallback);
+    if (document.readyState == "loading") {
+        document.addEventListener("readystatechange", runCallback);
     } else {
         runCallback();
     }
 }
 
 domReady(() => {
-    let elements = document.querySelectorAll('.carbon-dialog');
-
-    for (let index = 0; index < elements.length; index++) {
-        dialogues.push(new A11yDialog(elements[index]));
-    }
+    [...document.querySelectorAll(".carbon-dialog")].forEach((dialog) => {
+        dialogues.push(new A11yDialog(dialog));
+    });
 });
 
 export default dialogues;
